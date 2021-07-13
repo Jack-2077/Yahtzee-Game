e7ccbc0f02450b41b6151b17f85dd8f69922daf4
Jack <78120697+Jack-2077@users.noreply.github.com>
2021-07-12 21:18:36 -0700

    Add SmallStraight logic

--- src/Rules.js
+++ src/Rules.js
@@ -67,8 +67,17 @@ class FullHouse extends Rule {
 
 /** Check for small straights. */
 
-class SmallStraight {
-  // TODO
+class SmallStraight extends Rule {
+  evalRoll = (dice) => {
+    const d = new Set(dice);
+    if (d.has(2) && d.has(3) && d.has(4) && (d.has(1) || d.has(5)))
+      return this.score;
+
+    if (d.has(3) && d.has(4) && d.has(5) && (d.has(2) || d.has(6)))
+      return this.score;
+
+    return 0;
+  };
 }
 
 /** Check for large straights. */
@@ -107,7 +116,7 @@ const fourOfKind = new SumDistro({ count: 4 });
 const fullHouse = new FullHouse({ score: 25 });
 
 // small/large straights score as 30/40
-const smallStraight = "TODO";
+const smallStraight = new SmallStraight({ score: 30 });
 const largeStraight = new LargeStraight({ score: 40 });
 
 // yahtzee scores as 50
